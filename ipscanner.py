116a75053531b916a9803c01dd1245e62b4c59dd
huseyinguzel <huseyinguzel11@outlook.com>
2016-04-12 09:08:44 +0300

    veritabani_calısır_halde
    a

--- ipscanner.py
+++ ipscanner.py
@@ -1,30 +1,33 @@
 import subprocess
 import shlex
+import os
+import sys
+import time
 import pymysql
 
 
-def sqli(a):
+def sqli(ipadres):
         conn = pymysql.connect(
         db='python',
         user='root',
         passwd='1qazXSW2',
         host='localhost')
         c = conn.cursor()
-        c.execute("insert into python.ip(ipcol) values(%s)",a)
+        c.execute("insert into python.ip(ipcol) values(%s)",ipadres)
         conn.commit()
         conn.close()
 
-
 def main():
         for ip in range(1,255):
-             ipadres="192.168.31."+str(ip)
-             command_line = "ping -c 3 "+str(ipadres)
+             command_line = "ping -c 3 192.168.43."+str(ip)
              args = shlex.split(command_line)
+             ipadres="192.168.43."+str(ip)
              try:
                  subprocess.check_call(args,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-                 print ("ip canlı:"+"192.168.31."+str(ip))
+                 print ("ip canlı:"+"192.168.43."+str(ip))
                  dosya=open("ip.txt","a")
                  dosya.write(ipadres+"\n")
+                 time.sleep(1)
                  dosya.close()
                  conn = pymysql.connect(
                  db='python',
@@ -34,9 +37,18 @@ def main():
                  c = conn.cursor()
                  sorgu=c.execute("SELECT ipcol  FROM python.ip where ipcol=""%s",ipadres)
                  print(sorgu)
+                 if sorgu==0:
+                     print("veritabanında yok")
+                     cevap=input("eklemek için evet yazın devam etmek için entera basın:")
+                     if cevap=="evet":
+                         sqli(ipadres)
+                     else:
+                         print("eklenmedi")
+                 else:
+                     print("var")
+                 conn.close()
              except subprocess.CalledProcessError:
-                 print ("ip pinge ecevap vermiyor:"+str(ipadres))
+                 print ("ip pinge cevap vermiyor:"+"192.168.43."+str(ip))
+
 
 main()
-
-
